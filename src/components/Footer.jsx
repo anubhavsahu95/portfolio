41c4e37a1774213abe352011295e1f8c654beb53
Anubhav Sahu <100119761+anubhavsahu95@users.noreply.github.com>
2024-07-09 13:10:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <div class="text-center p-6 bg-black footer">
-        <span class="text-white">&copy; 2023 Copyright - </span>
+        <span class="text-white">&copy; 2024 Copyright - </span>
         <a class="link-underline text-white font-bold" href="https://anubhavsahu95.github.io/portfolio/">Anubhav Sahu</a>
       </div>
     )
