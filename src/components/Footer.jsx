1a0cdfa1dff06fff6749f3a3a97f9cccfe3762ed
Anubhav Sahu <anubhavsahu95@gmail.com>
2024-07-09 13:06:40 +0530

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <div class="text-center p-6 bg-black footer">
         <span class="text-white">&copy; 2023 Copyright - </span>
-        <a class="link-underline text-white font-bold" href="https://anubhavsahu.netlify.app">Anubhav Sahu</a>
+        <a class="link-underline text-white font-bold" href="https://anubhavsahu95.github.io/portfolio/">Anubhav Sahu</a>
       </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav  backdrop-blur-sm bg-opacity-50">
       <div>
-        <h1 className="text-5xl font-signature ml-2"><a className="link-underline link-underline-black" href="https://anubhavsahu.netlify.app" target="_blank" rel="noreferrer">Anubhav</a></h1>
+        <h1 className="text-5xl font-signature ml-2"><a className="link-underline link-underline-black" href="https://anubhavsahu95.github.io/portfolio/" target="_blank" rel="noreferrer">Anubhav</a></h1>
       </div>
 
       <ul className="hidden md:flex">
